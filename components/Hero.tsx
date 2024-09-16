25e143d748b2f82881d18d308b0c201b2de0041e
aln1234 <albinlamichhane9@gmail.com>
2024-09-16 22:06:48 +0300

    added hero section along with gsap animation for the firstname and last name

---
+++ components/Hero.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import React from "react";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+
+const Hero = () => {
+  const component = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      // create as many GSAP animations and/or ScrollTriggers here as you want...
+      gsap
+        .timeline()
+        .fromTo(
+          ".name-animation",
+          { x: -100, opacity: 0, rotate: -10 },
+          {
+            x: 0,
+            opacity: 1,
+            rotate: 0,
+
+            ease: "elastic.out(1,0.3)",
+            duration: 1,
+            transformOrigin: "left top",
+            stagger: { each: 0.1, from: "random" },
+          }
+        )
+        .fromTo(
+          ".job-title",
+          {
+            y: 20,
+            opacity: 0,
+            scale: 1.2,
+          },
+          {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            scale: 1,
+            ease: "elastic.out(1,0.3)",
+          }
+        );
+    }, component);
+    return () => ctx.revert(); // cleanup!
+  }, []);
+
+  const renderLetters = (name: string, key: string) => {
+    if (!name) return;
+    return name.split("").map((letter, index) => (
+      <span
+        key={index}
+        className={`name-animation name-animation-${key}-index inline-block opacity-0`}
+      >
+        {letter}
+      </span>
+    ));
+  };
+
+  return (
+    <div ref={component}>
+      <div className="grid min-h-[70vh] grid-cols-1 items-center md:grid-cols-2">
+        <div className="col-start-1 md:row-start-1" data-speed=".2">
+          <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter">
+            <span className="block text-slate-300">
+              {renderLetters("Albin", "first")}
+            </span>
+            <span className="-mt-[.2em] block text-slate-500  ">
+              {renderLetters("Lamichhane", "last")}
+            </span>
+          </h1>
+          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">
+            Front-end Developer
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
