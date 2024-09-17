4f57bcd572d1ae396e58c14cdf3ba5754d000f5d
aln1234 <albinlamichhane9@gmail.com>
2024-09-17 22:20:41 +0300

    added avatar animation

---
+++ components/Avatar.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import clsx from "clsx";
+import gsap from "gsap";
+import Image from "next/image";
+import React, { useEffect, useRef } from "react";
+
+const Avatar = () => {
+  const component = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.fromTo(
+        ".avatar",
+        {
+          opacity: 0,
+          scale: 1.4,
+        },
+        {
+          scale: 1,
+          opacity: 1,
+          duration: 1.3,
+          ease: "power3.inOut",
+        }
+      );
+
+      window.onmousemove = (e) => {
+        if (!component.current) return; // no component, no animation!
+        const componentRect = (
+          component.current as HTMLElement
+        ).getBoundingClientRect();
+        const componentCenterX = componentRect.left + componentRect.width / 2;
+
+        const componentPercent = {
+          x: (e.clientX - componentCenterX) / componentRect.width / 2,
+        };
+
+        const distFromCenterX = 1 - Math.abs(componentPercent.x);
+
+        gsap
+          .timeline({
+            defaults: { duration: 0.5, overwrite: "auto", ease: "power3.out" },
+          })
+          .to(
+            ".avatar",
+            {
+              rotation: gsap.utils.clamp(-2, 2, 5 * componentPercent.x),
+              duration: 0.5,
+            },
+            0
+          )
+          .to(
+            ".highlight",
+            {
+              opacity: distFromCenterX - 0.7,
+              x: -10 + 20 * componentPercent.x,
+              duration: 0.5,
+            },
+            0
+          );
+      };
+    }, component);
+    return () => ctx.revert(); // cleanup!
+  }, []);
+
+  return (
+    <div
+      className={clsx("relative h-full w-full cursor-pointer")}
+      ref={component}
+    >
+      <div
+        className="avatar aspect-square overflow-hidden rounded-3xl border-2 border-slate-700 opacity-0"
+        style={{ perspective: "500px", perspectiveOrigin: "150% 150%" }}
+      >
+        <Image
+          src="/profile.jpg"
+          alt="profile-picture"
+          width={500}
+          height={500}
+          className="avatar-image h-full w-full object-fill"
+        />
+
+        <div className="highlight absolute inset-0 hidden w-full scale-110 bg-gradient-to-tr from-transparent via-white to-transparent opacity-0 md:block"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Avatar;
